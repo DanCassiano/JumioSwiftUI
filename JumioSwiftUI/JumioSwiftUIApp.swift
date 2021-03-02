327986cefca1a8830f838c830797dfe5ad6ff646
DanCassiano <jordan_gnr@hotmail.com>
2021-03-02 12:27:55 -0300

    Initial Commit

---
+++ JumioSwiftUI/JumioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumioSwiftUIApp.swift
+//  JumioSwiftUI
+//
+//  Created by Jordan Cassiano on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct JumioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
